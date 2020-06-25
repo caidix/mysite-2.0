26bcf2e3d36276aefd9e7bcbea2ac7e358201e4b
CD <48509493+caidix@users.noreply.github.com>
2020-06-25 20:04:50 +0800

    打包admin

---
+++ admin/js/chunk-2d23721d.7ba86850.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d23721d"],{fa8d:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" Home ")])},c=[],l={},a=l,o=t("2877"),r=Object(o["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
---
+++ admin/js/chunk-744e5eb7.f3cfaecf.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-744e5eb7"],{"19aa5":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" About ")])},a=[],c={},l=c,o=t("2877"),r=Object(o["a"])(l,u,a,!1,null,null,null);n["default"]=r.exports}}]);
